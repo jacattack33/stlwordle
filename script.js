d931fdb42dd4b58c3b0e8be689705e217515068d
Jacque <jacquemueller2468@gmail.com>
2023-09-11 13:43:07 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,225 @@
+import { WORDS } from "./words.js";
+import { correctWORDS } from "./words.js";
+
+//Initialises the global variables for our game and picks 
+//a random word from the array WORDS as the right guess for this round. 
+//We also log the right guess to the console, to debug our code if necessary.
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = correctWORDS[Math.floor(Math.random() * correctWORDS.length)]
+console.log(rightGuessString)
+
+//initBoard creates one row for each guess and creates 5 boxes for each row. 
+//There is one box for each letter of the guess, and the function makes them all children of the row.
+//initBoard then adds each row to the board container. Each row is given the class 
+//letter-row, and each box is assigned letter-box.
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+initBoard()
+
+//This function checks that there's space in the guess for this letter, finds the appropriate row, 
+//and puts the letter in the box
+function insertLetter(pressedKey) {
+    if (nextLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter]
+    animateCSS(box, "pulse")
+    box.textContent = pressedKey
+    box.classList.add("filled-box")
+    currentGuess.push(pressedKey)
+    nextLetter += 1
+}
+
+//Function gets the correct row, finds the last box and empties it, then resets the nextLetter counter
+function deleteLetter() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    currentGuess.pop()
+    nextLetter -= 1
+}
+
+//Function checks guess is 5 letters, guess is valid list, checks letter and shade correctly, tell user about game end
+function checkGuess() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let guessString = ''
+    let rightGuess = Array.from(rightGuessString)
+
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    if (guessString.length != 5) {
+        toastr.error("Not enough letters!")
+        return
+    }
+
+    if (!WORDS.includes(guessString)) {
+        toastr.error("Word not in list!")
+        return
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letterColor = ''
+        let box = row.children[i]
+        let letter = currentGuess[i]
+
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        // is letter in the correct guess
+        if (letterPosition === -1) {
+            letterColor = 'gray'
+        } else {
+            // now, letter is definitely in word
+            // if letter index and right guess index are the same
+            // letter is in the right position 
+            if (currentGuess[i] === rightGuess[i]) {
+                // shade green 
+                letterColor = 'green'
+            } else {
+                // shade box yellow
+                letterColor = 'goldenrod'
+            }
+
+            rightGuess[letterPosition] = "#"
+        }
+
+        let delay = 250 * i
+        setTimeout(() => {
+            //flip box
+            animateCSS(box, 'flipInX')
+            //shade box
+            box.style.backgroundColor = letterColor
+            shadeKeyBoard(letter, letterColor)
+        }, delay)
+    }
+
+    if (guessString === rightGuessString) {
+        toastr.success("You guessed right! Game over!")
+        guessesRemaining = 0
+        return
+    } else {
+        guessesRemaining -= 1;
+        currentGuess = [];
+        nextLetter = 0;
+
+        if (guessesRemaining === 0) {
+            toastr.error("You've run out of guesses! Game over!")
+            toastr.info(`The right word was: "${rightGuessString}"`)
+        }
+    }
+}
+
+//receives the letter to shade and the color we want to shade it
+//Find the key that matches the given letter
+//If the key is already green, do nothing
+//If the key is currently yellow, only allow it to become green
+//Else, shade the key passed to the function
+function shadeKeyBoard(letter, color) {
+    for (const elem of document.getElementsByClassName("keyboard-button")) {
+        if (elem.textContent === letter) {
+            let oldColor = elem.style.backgroundColor
+            if (oldColor === 'green') {
+                return
+            }
+
+            if (oldColor === 'goldenrod' && color !== 'green') {
+                return
+            }
+
+            elem.style.backgroundColor = color
+            break
+        }
+    }
+}
+
+
+
+//checks that the key pressed was alphabetical single letter. 
+//If name of key doesn't have letter (number), or has mult letters (Shift, Tab), we ignore the event. 
+//Otherwise, we insert the letter into the board.
+document.addEventListener("keyup", (e) => {
+
+    if (guessesRemaining === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+//function listens for click on keyboard container or any of its children(the buttons)
+//if clicked element not a button - exit function
+//else, dispatch key up event corresponding to clicked key
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+    const target = e.target
+
+    if (!target.classList.contains("keyboard-button")) {
+        return
+    }
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    }
+
+    document.dispatchEvent(new KeyboardEvent("keyup", { 'key': key }))
+})
+
+//function from animate homepage - applies classes to animation target to trigger animation
+//once ended - removes added classes
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        // const node = document.querySelector(element);
+        const node = element
+        node.style.setProperty('--animate-duration', '0.3s');
+
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
